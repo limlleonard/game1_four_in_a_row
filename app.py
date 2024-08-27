95c1e7520cb63ac695744547f025cad8a90d67db
limlleonard <limlleonard@gmail.com>
2024-08-27 11:25:25 +0200

    draw board and draw pieces

--- app.py
+++ app.py
@@ -166,6 +166,48 @@ pg.init()
 pg.display.set_caption("Four in a row")
 screen = pg.display.set_mode((SQ * C, SQ * (R + 1)))
 stage = 1
+g1 = Game(1)
+
+
+def draw_board(stage):
+    for x in range(C):
+        for y in range(R):
+            pg.draw.circle(
+                screen, "#CDC0B4", (x * SQ + SQ // 2, y * SQ + SQ // 2), SQ // 3, 4
+            )
+    if stage == 1:
+        guide1 = pg.font.SysFont("Arial", 30).render(start_text1, False, (0, 255, 0))
+        screen.blit(guide1, (SQ // 4, SQ * R))
+        guide1 = pg.font.SysFont("Arial", 30).render(start_text2, False, (0, 255, 0))
+        screen.blit(guide1, (SQ // 4, SQ * R + SQ // 2))
+    elif stage == 2:
+        pass
+    elif stage == 3:
+        guide1 = pg.font.SysFont("Arial", 30).render(win_tf, False, (0, 255, 0))
+        screen.blit(guide1, (SQ // 4, SQ * R))
+        guide1 = pg.font.SysFont("Arial", 30).render("Again?(0/1)", False, (0, 255, 0))
+        screen.blit(guide1, (SQ // 4, SQ * R + SQ // 2))
+
+
+def draw_pieces(board):
+    for x in range(C):  # Draw board
+        for y in range(R):
+            if board[y][x] == 1:
+                pg.draw.circle(
+                    screen,
+                    (200, 0, 0),
+                    (x * SQ + SQ // 2, y * SQ + SQ // 2),
+                    SQ // 3,
+                )
+            if board[y][x] == 2:
+                pg.draw.circle(
+                    screen,
+                    (0, 200, 200),
+                    (x * SQ + SQ // 2, y * SQ + SQ // 2),
+                    SQ // 3,
+                )
+
+
 while play:
     clock.tick(40)
     screen.fill((0, 0, 0))
@@ -195,36 +237,7 @@ while play:
                 mx, my = pg.mouse.get_pos()
                 g1.move_game(mx // SQ)
             pass
-    for x in range(C):  # Draw board
-        for y in range(R):
-            pg.draw.circle(
-                screen, "#CDC0B4", (x * SQ + SQ // 2, y * SQ + SQ // 2), SQ // 3, 4
-            )
-            if stage > 1:  # shows result even it is finished
-                if g1.board[y][x] == 1:
-                    pg.draw.circle(
-                        screen,
-                        (200, 0, 0),
-                        (x * SQ + SQ // 2, y * SQ + SQ // 2),
-                        SQ // 3,
-                    )
-                if g1.board[y][x] == 2:
-                    pg.draw.circle(
-                        screen,
-                        (0, 200, 200),
-                        (x * SQ + SQ // 2, y * SQ + SQ // 2),
-                        SQ // 3,
-                    )
-    if stage == 1:
-        guide1 = pg.font.SysFont("Arial", 30).render(start_text1, False, (0, 255, 0))
-        screen.blit(guide1, (SQ // 4, SQ * R))
-        guide1 = pg.font.SysFont("Arial", 30).render(start_text2, False, (0, 255, 0))
-        screen.blit(guide1, (SQ // 4, SQ * R + SQ // 2))
-    elif stage == 2:
-        pass
-    elif stage == 3:
-        guide1 = pg.font.SysFont("Arial", 30).render(win_tf, False, (0, 255, 0))
-        screen.blit(guide1, (SQ // 4, SQ * R))
-        guide1 = pg.font.SysFont("Arial", 30).render("Again?(0/1)", False, (0, 255, 0))
-        screen.blit(guide1, (SQ // 4, SQ * R + SQ // 2))
+    draw_board(stage)
+    if stage > 1:  # shows result even it is finished
+        draw_pieces(g1.board)
     pg.display.flip()
